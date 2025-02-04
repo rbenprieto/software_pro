6fbd96056b238fdb13c27b66b22d7b51af0e7d7e
rbenprieto <ruben.prieto@fracttal.com>
2025-02-04 08:49:18 -0500

    partial deploy

--- software_pro/settings.py
+++ software_pro/settings.py
@@ -125,7 +125,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
